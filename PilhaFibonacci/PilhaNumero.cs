9327769adb857f5e7759a35176a90d3ba6c89b23
Gustavo <guvono4@gmail.com>
2024-05-04 10:23:27 -0300

    Add project files.

---
+++ PilhaFibonacci/PilhaNumero.cs
@@ -0,0 +1,48 @@
+﻿namespace PilhaFibonacci
+{
+    internal class PilhaNumero
+    {
+        Numero? topo;
+        int contador;
+
+        public PilhaNumero()
+        {
+            this.Topo = null;
+        }
+
+        internal Numero? Topo { get => topo; set => topo = value; }
+
+        public void Push(Numero n)
+        {
+            Numero? aux = this.topo;
+            this.topo = n;
+            n.SetAnterior(aux!);
+            this.contador++;
+        }
+
+        public void Print()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine($"Pilha vazia! Pressione qualquer tecla para continuar...");
+                Console.ReadKey();
+            }
+            else
+            {
+                Numero? aux = this.Topo;
+                do
+                {
+                    Console.WriteLine($"Posição na pilha: {this.contador} - Numero: {aux!.GetN()}");
+                    this.contador--;
+                    aux = aux.GetAnterior();
+                } while (aux != null);
+            }
+        }
+
+        bool IsEmpty()
+        {
+            if (this.topo == null) return true;
+            else return false;
+        }
+    }
+}
---
+++ PilhaFibonacci/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace PilhaFibonacci;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int restart = 0, numero = 1, ultimo = 1, penultimo = -1, atual = 0;
+        PilhaNumero pilha = new();
+
+        do
+        {
+            ultimo = 1;
+            penultimo = -1;
+            atual = 0;
+
+            do
+            {
+                Console.Clear();
+
+                if (numero <= 0) Console.WriteLine("ERRO: Informe um número maior que 0");
+
+                Console.Write("Informe quantos números da sequência fibonacci você deseja adicionar na pilha:");
+                numero = int.Parse(Console.ReadLine());
+            } while (numero <= 0);
+
+            for (int i = 0; i < numero; i++)
+            {
+                atual = ultimo + penultimo;
+                pilha.Push(new Numero(atual));
+                penultimo = ultimo;
+                ultimo = atual;
+            }
+
+            pilha.Print();
+
+            Console.WriteLine("\nDeseja reiniciar o programa?");
+            Console.WriteLine("1 - SIM");
+            Console.WriteLine("0 - NAO");
+            restart = int.Parse(Console.ReadLine());
+            if (restart != 0) pilha = new();
+        } while (restart != 0);
+    }
+}
